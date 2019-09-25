6d85a6d9cb8dd586e2137d6a6286d47f6f751a9a
i-Krypt <vamay505@gmail.com>
2019-09-25 15:25:16 +0300

    classes for the blog

--- blog/models.py
+++ blog/models.py
@@ -2,34 +2,36 @@ from datetime import datetime
 from blog import db, login_manager
 from flask_login import UserMixin
 
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
 
 class User(db.Model, UserMixin):
-        id = db.Column(db.Integer, primary_key=True)
-        username = db.Column(db.String(20), unique=True, nullable=False)
-        email = db.Column(db.String(50), unique=True, nullable=False)
-        password = db.Column(db.String(25), nullable=False)
-        post = db.relationship('Post', backref='author', lazy=True)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(25), nullable=False)
+    post = db.relationship('Post', backref='author', lazy=True)
 
-        def __repr__(self):
-            return f"User('{self.username}', '{self.email}')"
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}')"
 
 
 class Post(db.Model):
-        id = db.Column(db.Integer, primary_key=True)
-        title = db.Column(db.String(150), nullable=False)
-        date_posted = db.Column(db.DateTime, nullable=False,default=datetime.utcnow)
-        content = db.Column(db.Text, nullable=False)
-        user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False,
+                            default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
-        def __repr__(self):
-            return f"Post('{self.title}', '{self.date_posted}')"
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
 
 
 class Quote():
-   def __init__(self, author,quoteMsg):
-       self.author = author
-       self.quoteMsg = quoteMsg
+    def __init__(self, author, quoteMsg):
+        self.author = author
+        self.quoteMsg = quoteMsg
