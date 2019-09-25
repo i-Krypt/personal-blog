4cfc925a93e1f7b8e559d732c266bac98789d573
i-Krypt <vamay505@gmail.com>
2019-09-25 12:06:17 +0300

    deploy

--- blog/routes.py
+++ blog/routes.py
@@ -3,7 +3,7 @@ from blog import app, db, bcrypt
 from blog.forms import RegistrationForm, LoginForm, PostForm
 from blog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
-from request import get_quotes
+from requests import get_quotes
 
 
 @app.route('/')
