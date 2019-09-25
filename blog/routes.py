a5d3d845092335f073dcd8f6899dcc715d0db10f
i-Krypt <vamay505@gmail.com>
2019-09-25 15:26:37 +0300

    made routes for the app

--- blog/routes.py
+++ blog/routes.py
@@ -5,15 +5,18 @@ from blog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 from request import get_quotes
 
+
 @app.route('/')
 def home():
     quotes = get_quotes()
     posts = Post.query.all()
     return render_template('home.html', posts=posts, quotes=quotes)
 
+
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
 
 @app.route("/comments")
 def comments():
@@ -26,15 +29,17 @@ def register():
         return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        hashed_password = bcrypt.generate_password_hash(
+            form.password.data).decode('utf-8')
+        user = User(username=form.username.data,
+                    email=form.email.data, password=hashed_password)
         db.session.add(user)
         db.session.commit()
         flash('Account has been created! You are now able to login', 'success')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
-# LoginForm route   
+# LoginForm route
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
@@ -50,35 +55,38 @@ def login():
             flash('login unsuccessful', 'danger')
     return render_template('login.html', title='Login', form=form)
 
+
 @app.route("/logout")
 def logout():
     logout_user()
     return redirect(url_for('home'))
+
 
 @app.route("/account")
 @login_required
 def account():
     return render_template('account.html', title='Account')
 
+
 @app.route("/post/new", methods=['GET', 'POST'])
 @login_required
 def new_post():
     form = PostForm()
     if form.validate_on_submit():
-        post = Post(title=form.title.data, content=form.content.data, author=current_user)
+        post = Post(title=form.title.data,
+                    content=form.content.data, author=current_user)
         db.session.add(post)
         db.session.commit()
         flash('Your post has been created', 'success')
         return redirect(url_for('home'))
-    return render_template('create_post.html', title='New Post', 
-    form=form, legend='Update Post')
+    return render_template('create_post.html', title='New Post',
+                           form=form, legend='Update Post')
 
 
 @app.route("/post/<int:post_id>")
 def post(post_id):
     post = Post.query.get_or_404(post_id)
     return render_template('post.html', title=post.title, post=post)
-
 
 
 @app.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
@@ -96,8 +104,8 @@ def update_post(post_id):
     elif request.method == 'GET':
         form.title.data = post.title
         form.content.data = post.content
-    return render_template('create_post.html', title='Update Post', 
-    form=form, legend='Update Post')
+    return render_template('create_post.html', title='Update Post',
+                           form=form, legend='Update Post')
 
 
 @app.route("/post/<int:post_id>/delete", methods=['POST'])
